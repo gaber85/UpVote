a0c3c0395c5a041256abb79df0df6654e047a9a3
gaber85 <gabrielr85@gmail.com>
2018-12-13 18:17:43 +0100

    Styling complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,32 @@ import './App.css';
 
 class App extends React.Component {
   render() {
-    return;
+    return (
+      <div className="container">
+        <div className="addTopic">
+          <form>
+            <input type="text" className="input-bar" placeholder="add new topic..."></input>
+            <button type="submit" className="accessory-button">+</button>
+          </form>
+        </div>
+        <div className="topicList">
+          <div className="topicItem">
+            <div className="votingComp">
+              <button className="accessory-button">⬆</button>
+              <div className="voteCounter">4</div>
+              <button className="accessory-button">⬇</button>
+            </div>
+            <div className="topicInfo">
+              <div className="content">Topic content goes here</div>
+              <div className="date">Created On <b> Date goes here</b></div>
+            </div>
+            <div>
+              <button className="accessory-button">🗑</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
