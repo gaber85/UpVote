7cb5039984d681dcca7445723dda93ad0772f50f
gaber85 <gabrielr85@gmail.com>
2018-12-28 16:55:24 +0100

    added comment to controller.vote function

--- server/controller.js
+++ server/controller.js
@@ -60,6 +60,7 @@ exports.vote = async (ctx) => {
   try {
     const id = ctx.params.id;
     const direction = ctx.params.direction;
+    // need to add the option to findOneAndUpdate {useFindAndModify: false} because it uses modify by default
     direction === 'up' ? await Topic.findOneAndUpdate({ _id : id }, {$inc : { score : 1}}, {useFindAndModify: false})
       : await Topic.findOneAndUpdate({ _id : id }, {$inc : { score : -1}}, {useFindAndModify: false});
     console.log('Voted: ', direction); // eslint-disable-line no-console
